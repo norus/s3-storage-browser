00908802af892b56834b33c533e6ecb25a3d5660
Ruslan Valiyev <ruslan@valiyev.com>
2025-01-03 22:02:39 +0100

    fix(storage): update access policy for S3 bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 's3uploadbucket',
   access: (allow) => ({
-    '/*': [
+    'upload/*': [
       allow.authenticated.to(['read', 'write', 'delete'])
     ]
   })
