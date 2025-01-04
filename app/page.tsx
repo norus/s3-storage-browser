4e977fe8380522b994b2e2a00f6370e49a5c82ef
Ruslan Valiyev <ruslan@valiyev.com>
2025-01-04 22:37:27 +0100

    refactor: remove unused getUserAttributes function

--- app/page.tsx
+++ app/page.tsx
@@ -5,44 +5,22 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import { fetchUserAttributes } from 'aws-amplify/auth';
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react';
 import { StorageBrowser } from '../components/StorageBrowser';
 
-// Configure Amplify
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-// Define the CognitoUser interface
-interface CognitoUser {
-  username?: string;
-  attributes?: {
-    'custom:firstname'?: string;
-    [key: string]: any;
-  };
-}
-
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [firstName, setFirstName] = useState<string>("");
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }
-
-  async function getUserAttributes() {
-    try {
-      const attributes = await fetchUserAttributes();
-      console.log('User attributes:', attributes);
-      setFirstName(attributes['custom:firstname'] || '');
-    } catch (error) {
-      console.error('Error fetching user attributes:', error);
-    }
   }
 
   useEffect(() => {
@@ -60,28 +38,17 @@ export default function App() {
       initialState="signIn"
       hideSignUp={true}
     >
-      {({ signOut, user }) => {
-        const cognitoUser = user as unknown as CognitoUser;
-        console.log('Full user object:', user);
-        
-        // Fetch attributes when user is available
-        useEffect(() => {
-          if (user) {
-            getUserAttributes();
-          }
-        }, [user]);
-        
-        return (
-          <main>
-            <h1>Hello {firstName || cognitoUser?.username}</h1>
+      {({ signOut, user }) => (
+        <main>
+            <h1>Hello {user?.signInDetails?.loginId}</h1>
             <button onClick={signOut}>Sign out</button>
 
-            {/* StorageBrowser Component */}
-            <h2>Your Files</h2>
-            <StorageBrowser />
-          </main>
-        );
-      }}
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser />
+
+        </main>
+      )}
     </Authenticator>
   );
 }
