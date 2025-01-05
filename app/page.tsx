afafb403bbedd6a3370c4d458bf43657cdab2910
Ruslan Valiyev <ruslan@valiyev.com>
2025-01-05 12:54:44 +0100

    refactor: remove unused data resource

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -12,27 +9,7 @@ import { StorageBrowser } from '../components/StorageBrowser';
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
   return (
     <Authenticator
       initialState="signIn"
